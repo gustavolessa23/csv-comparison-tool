9e6ba5b748ccdbddd59a814b40638d23b4fdabcc
Gustavo Lessa <gustavolessadublin@gmail.com>
2020-07-03 06:35:48 +0100

    FIX: creating directory if output path doesn't exist.

--- src/main/java/com/gustavolessa/csvcomparisontool/ReportWriter.java
+++ src/main/java/com/gustavolessa/csvcomparisontool/ReportWriter.java
@@ -3,16 +3,24 @@ package com.gustavolessa.csvcomparisontool;
 import com.opencsv.CSVWriter;
 
 import java.io.FileWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class ReportWriter {
 
     public static void writeReport(List<String[]> report, Path path) throws Exception {
+
+        try {
+            Files.createDirectories(path.getParent());
+            Files.createFile(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         CSVWriter writer = new CSVWriter(new FileWriter(path.toString()));
         writer.writeAll(report);
 
         writer.close();
-//        return Helpers.readFile(path);
     }
 }
